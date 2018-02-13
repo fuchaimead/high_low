68ec601912415c88593079e13dc175dba8c33110
FuChai Mead <mead.fuchai@gmail.com>
2018-02-13 00:29:14 -0700

    working high low game

--- high_low.rb
+++ high_low.rb
@@ -14,23 +14,27 @@ class HighLow
     puts "How much would you like to bet?"
     bet = gets.to_i 
     first_card = @deck.cards.sample 
-    numeric_value(first_card)
+    card1 = numeric_value(first_card)
     puts "#{first_card.rank} of #{first_card.suit}"
     puts "Is the next card higher or lower?"
     answer = gets.strip
     second_card = @deck.cards.sample
-    numeric_value(second_card)
+    card2 = numeric_value(second_card)
     puts "The second card is #{second_card.rank} of #{second_card.suit}"
     if answer == "higher"
-      if first_card.rank > second_card.rank
+      if card1 < card2
         puts "You win"
-      else 
+      elsif card1 == card2
+        puts "It's a draw"
+      else
         puts "You lose"
       end
     elsif answer == "lower"
-      if first_card.rank < second_card.rank
+      if card1 > card2
         puts "You win"
-      else 
+      elsif card1 == card2
+        puts "It's a draw"
+      else
         puts "You lose"
       end
     end 
@@ -39,7 +43,7 @@ class HighLow
 
 
  def numeric_value(card)
-  faces = {'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14}
+  faces = { 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14 }
   begin
     if Integer(card.rank).is_a?(Numeric)
       value = card.rank.to_i
