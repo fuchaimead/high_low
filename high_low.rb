be99adfd2cd51368d9cbc61f9d60d5117e39bc29
FuChai Mead <mead.fuchai@gmail.com>
2018-02-12 22:52:35 -0700

    card logic

--- high_low.rb
+++ high_low.rb
@@ -15,7 +15,22 @@ class HighLow
     puts "#{first_card.rank} of #{first_card.suit}"
     puts "Is the next card higher or lower?"
     answer = gets.strip
-    
+    second_card = @deck.cards.sample
+    puts "The second card is #{second_card.rank} of #{second_card.suit}"
+    if answer == "higher"
+      if first_card.rank > second_card.rank
+        puts "You lose"
+      else 
+        puts "You win"
+      end
+    elsif answer == "lower"
+      if first_card.rank < second_card.rank
+        puts "You win"
+      else 
+        puts "You lose"
+      end
+    end 
+    game
   end 
 
 end 
